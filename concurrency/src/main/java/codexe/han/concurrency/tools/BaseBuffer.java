8d5da031d5009814420ffa76856632e593972c9a
Zhenchao Bing <bingzhenchao@mozat.com>
2019-04-15 21:00:34 +0800

    conditional base buffer

--- concurrency/src/main/java/codexe/han/concurrency/tools/BaseBuffer.java
+++ concurrency/src/main/java/codexe/han/concurrency/tools/BaseBuffer.java
@@ -258,3 +258,130 @@ class BufferLockCondition<T> extends BaseBuffer<T>{
         consumer2.interrupt();
     }
 }
+
+class BaseBufferLockCondition<T>{
+    private final T[] buffer;
+
+    private int count;
+
+    public BaseBufferLockCondition(int capacity){
+        buffer = (T[])(new Object[capacity]);
+    }
+    private Lock lock = new ReentrantLock();
+    private Condition notFull = lock.newCondition();
+    private Condition notEmpty = lock.newCondition();
+
+    public int head;
+    private int tail;
+
+    public void put(T t) throws InterruptedException {
+        lock.lock();
+        try {
+            while (isFull()) {
+                notFull.await();//在not full 条件队列上等待 condition queue
+            }
+            boolean wasEmpty = isEmpty();//空
+            buffer[tail] = t;//非空
+            System.out.println("put "+t);
+            count++;
+            if (++tail == buffer.length) {
+                tail = 0;
+            }
+            if (wasEmpty) {//conditional signal
+                notEmpty.signalAll();
+            }
+       //     notEmpty.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public T take() throws InterruptedException {
+        lock.lock();
+        try{
+            while(isEmpty()){
+                notEmpty.await();//在 not empty 条件队列上等待 condition queue
+            }
+            boolean wasFull = isFull();//满
+            T t = buffer[head];//非满
+            System.out.println("take "+t);
+            buffer[head] = null;
+            count--;
+            if(++head==buffer.length){
+                head = 0;
+            }
+            if(wasFull){
+                notFull.signalAll();
+            }
+      //      notFull.signal();
+            return t;
+        } finally{
+            lock.unlock();
+        }
+    }
+
+    private boolean isFull(){
+        return count == buffer.length;
+    }
+    private boolean isEmpty(){
+        return count == 0;
+    }
+
+    public static void main(String[] args) {
+        BaseBufferLockCondition<Integer> bufferLockCondition = new BaseBufferLockCondition<Integer>(8);
+        Thread producer1 = new Thread(()->{
+            for(int i =0; i< 20; i++) {
+                try {
+                    bufferLockCondition.put(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+        Thread producer2 = new Thread(()->{
+            for(int i =20; i< 40; i++) {
+                try {
+                    bufferLockCondition.put(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
+
+        Thread consumer1 = new Thread(()->{
+            while(!Thread.currentThread().isInterrupted()){
+                try {
+                    int i = bufferLockCondition.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    break;
+                }
+            }
+        });
+        Thread consumer2 = new Thread(()->{
+            while(!Thread.currentThread().isInterrupted()){
+                try {
+                    int i = bufferLockCondition.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    break;
+                }
+            }
+        });
+        producer1.start();
+        producer2.start();
+        consumer1.start();
+        consumer2.start();
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        consumer1.interrupt();
+        consumer2.interrupt();
+    }
+}
