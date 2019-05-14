e90ca0029eff5b9cd7fb7e4e13437e62616eadf1
codexe <zhenchaobing@gmail.com>
2019-05-14 22:19:55 +0800

    daily escape

--- leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape20.java
+++ leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape20.java
@@ -5,18 +5,25 @@ import java.util.LinkedList;
 /**
  * 遇见左括号，可以入栈
  * 遇见右括号，一定是已经出错了！！！！
+ *
+ * LinkedList做stack
+ * push是在第一个元素之前添加，不是在list tail, 而是在list head之前
  */
 public class escape20 {
-    //TODO: Test
-    public boolean isValid(String s){
+    public static void main(String[] args) {
+        String s = "([]){}";
+        isValid(s);
+    }
+    public static boolean isValid(String s){
         LinkedList<Character> stack = new LinkedList<>();
         for(char ch : s.toCharArray()){
-            if(stack.peekLast()=='(' || stack.peekLast()=='['|| stack.peekLast()=='{'){
+            if(stack.isEmpty()||stack.peekFirst()=='(' || stack.peekFirst()=='['|| stack.peekFirst()=='{'){
                 if(ch == '(' || ch == '[' || ch == '{'){
                     stack.push(ch);
                 }
                 else{
-                    char stackTop = stack.pollLast();
+                    if(stack.isEmpty()) return false;
+                    char stackTop = stack.pollFirst();//假设栈最后只有一个元素，不能直接出栈
                     if (!((stackTop == '(' && ch == ')') || (stackTop == '[' && ch == ']') || (stackTop == '{' && ch == '}'))) {
                         return false;
                     }
---
+++ leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape21.java
@@ -0,0 +1,12 @@
+package codexe.han.leetcode.escapeplan;
+
+public class escape21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+    }
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+}
