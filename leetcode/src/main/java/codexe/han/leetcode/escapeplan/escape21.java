47d5e64133840d4fc9002e94b21aa1688122f26d
Zhenchao Bing <bingzhenchao@mozat.com>
2019-05-15 09:49:13 +0800

    daily escape

--- leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape21.java
+++ leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape21.java
@@ -1,7 +1,36 @@
 package codexe.han.leetcode.escapeplan;
 
+import lombok.val;
+
 public class escape21 {
+    //TODO:Test
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        head.next = null;
+        ListNode p = head;
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+        while(p1!=null && p2!=null){
+            if(p1.val < p2.val){
+                p.next = new ListNode(p1.val);
+                p1 = p1.next;
+            }
+            else{
+                p.next = new ListNode(p2.val);
+                p2 = p2.next;
+            }
+        }
+        /*if(p1!=null){
+            p.next = p1;
+            return head.next;
+        }
+        if(p2!=null){
+            p.next = p2;
+            return head.next;
+        }*/
+        //合并上面的if语句
+        p.next = p1!= null?p1:p2;
+        return head.next;
 
     }
     public class ListNode {
