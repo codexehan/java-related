662a01c11c8712678105e6e53e31b0d1035fa833
codexe <zhenchaobing@gmail.com>
2019-05-28 22:25:53 +0800

    daily escape

--- leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape70.java
+++ leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape70.java
@@ -1,7 +1,21 @@
 package codexe.han.leetcode.escapeplan;
 
+/**
+ * 动态规划
+ *
+ * n的路径 由n-1, n-2相加构成
+ */
 public class escape70 {
     public int climbStairs(int n) {
-
+        int n1 = 2;
+        int n2 = 1;
+        if(n==1) return 1;
+        if(n==2) return 2;
+        for(int i =3; i<=n; i++){
+            int tmp = n1;
+            n1 = n1+n2;
+            n2 = tmp;
+        }
+        return n1;
     }
 }
---
+++ leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape71.java
@@ -0,0 +1,7 @@
+package codexe.han.leetcode.escapeplan;
+
+public class escape71 {
+    public String simplifyPath(String path) {
+
+    }
+}
