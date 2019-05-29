18513823d65ca77d8a0ea29ea9abf41b8d8610fb
codexe <zhenchaobing@gmail.com>
2019-05-29 22:56:04 +0800

    daily escape

--- leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape71.java
+++ leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape71.java
@@ -1,7 +1,30 @@
 package codexe.han.leetcode.escapeplan;
 
-public class escape71 {
-    public String simplifyPath(String path) {
+import java.util.Arrays;
+import java.util.LinkedList;
 
+/**
+ * 根据'/'提取
+ */
+public class escape71 {
+    public static void main(String[] args) {
+        String p = "//home////ss/";
+        for(String s : p.split("/")){
+
+            System.out.println(s + ","+(s==""));
+        }
+    }
+    public String simplifyPath(String path) {
+        LinkedList<String> stack = new LinkedList<>();
+        int j=0;
+        for(int i=0;i<path.length();i++){
+            if(path.charAt(i)=='/'){
+                String folder = path.substring(j,i);
+                if(folder.equals(".")) continue;
+                if(folder.equals("..")) stack.pop();
+
+            }
+        }
+        return "";
     }
 }
