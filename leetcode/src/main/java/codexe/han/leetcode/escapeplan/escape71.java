0ef1ad97a38ad80d89b8d95aeff0608686f2dc28
codexe <zhenchaobing@gmail.com>
2019-05-30 22:54:55 +0800

    daily escape

--- leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape71.java
+++ leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape71.java
@@ -4,23 +4,23 @@ import java.util.Arrays;
 import java.util.LinkedList;
 
 /**
- * 根据'/'提取
+ * 根据'/'提取，但是要注意 末尾没有/的情况！！！！
+ *
+ * 还有一种方法是利用split进行
  */
 //TODO:Test
 public class escape71 {
     public static void main(String[] args) {
-        String p = "//home////ss/";
-        for(String s : p.split("/")){
-
-            System.out.println(s + ","+(s==""));
-        }
+        simplifyPath("/aa/..");
     }
-    public String simplifyPath(String path) {
+    public static String simplifyPath(String path) {
         LinkedList<String> stack = new LinkedList<>();
-        int j=0;
-        for(int i=0;i<path.length();i++){
-            if(path.charAt(i)=='/'){
+        int j=1;
+        int i=1;
+        for(;i<path.length();i++){
+            if(path.charAt(i)=='/'){//   xxx/  或者 /sss
                 String folder = path.substring(j,i);
+                j=i+1;
                 if(folder.isEmpty()||folder.equals(".")) continue;
                 if(folder.equals("..")){
                     stack.pollLast();
@@ -30,10 +30,23 @@ public class escape71 {
                 }
             }
         }
+        if(path.charAt(i-1)!='/'){//   xxx/  或者 /sss
+            String folder = path.substring(j,i);
+            j=i+1;
+            if(folder.isEmpty()||folder.equals(".")){
+
+            }
+            else if(folder.equals("..")){
+                stack.pollLast();
+            }
+            else{
+                stack.addLast("/"+folder);
+            }
+        }
         StringBuilder res = new StringBuilder();
         for(String r : stack){
             res.append(r);
         }
-        return res.toString();
+        return res.length()==0?"/":res.toString();
     }
 }
--- leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape73.java
+++ leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape73.java
@@ -2,33 +2,51 @@ package codexe.han.leetcode.escapeplan;
 
 /**
  * 为了节省空间，用第一行和第一列做记录
+ *
+ * 更新的时候要注意一个问题，最后再更新第一行 和 第一列
+ * 表示第一行和第一列的是同一个值[0][0]
+ * 原因是 假如先更新[0][0]==0 第一行，全部变为为0，那么更新列的时候，因为第一行的数值都变成0了 导致 每一列都变成0了
+ * 所以要单独记录是否更新第一行，第一列
  */
 //TODO: TEST
 public class escape73 {
     public void setZeroes(int[][] matrix){
+        boolean fr=false, fc=false;
         int m = matrix.length;
         int n = matrix[0].length;
         for(int i =0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(matrix[i][j]==0){
+                    if(i==0) fr=true;
+                    if(j==0) fc=true;
                     matrix[0][j] =0;//列
                     matrix[i][0] =0;//行
                 }
             }
         }
         //遍历列和行
-        for(int i =0; i<m; i++){
+        for(int i =1; i<m; i++){
             if(matrix[i][0]==0){//列为0
                 for(int j=0; j<n; j++){
                     matrix[i][j]=0;
                 }
             }
         }
-        for(int j=0; j<n; j++){
+        for(int j=1; j<n; j++){
             if(matrix[0][j]==0){//行为0
                 for(int i=0; i<m; i++){
                     matrix[i][j]=0;
                 }
+            }
+        }
+        if(fc){
+            for(int i=0; i<m; i++){
+                matrix[i][0]=0;
+            }
+        }
+        if(fr){
+            for(int j=0; j<n; j++){
+                matrix[0][j]=0;
             }
         }
     }
