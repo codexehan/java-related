afa7f6469638d73ec337c0c52aa16995d877b122
codexe <zhenchaobing@gmail.com>
2019-06-13 22:03:54 +0800

    daily escape

--- leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape90.java
+++ leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape90.java
@@ -11,13 +11,13 @@ public class escape90 {
     }
     public static List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
-        res.add(new ArrayList<>());
-        backtracking(nums,0,res);
-        return res;
+        List<List<Integer>> finalRes = new ArrayList<>();
+        finalRes.add(new ArrayList<>());
+        backtracking(finalRes,new ArrayList<>(),0,nums);
+        return finalRes;
     }
 
-    public static void backtracking(int[] nums, int start, List<List<Integer>> res){
+    /*public static void backtracking(int[] nums, int start, List<List<Integer>> res){
         if(start<nums.length) {
             if (!(start > 0 && nums[start] == nums[start - 1])||start==0) {
                 List<List<Integer>> tmp = new ArrayList<>();
@@ -29,6 +29,17 @@ public class escape90 {
                 res.addAll(tmp);
             }
             backtracking(nums, start + 1, res);
+        }
+    }*/
+
+    public static void backtracking(List<List<Integer>> finalRes,List<Integer> res, int start, int[] nums){
+        for(int i=start;i<nums.length;i++){
+            //每次校验相同必须从start后面的开始校验，因为start对应的数字，是必须要加入到结果集中的！！！！！！！
+            if(i>start&&nums[i]==nums[i-1]) continue;
+            res.add(nums[i]);
+            finalRes.add(new ArrayList<>(res));
+            backtracking(finalRes,res,i+1,nums);
+            res.remove(res.size()-1);
         }
     }
 }
