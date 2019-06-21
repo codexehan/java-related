9944191e15402615e7042ebf80c72bbfb2493e57
Zhenchao Bing <bingzhenchao@mozat.com>
2019-06-21 23:16:45 +0800

    daily escape

--- leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape110.java
+++ leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape110.java
@@ -1,5 +1,9 @@
 package codexe.han.leetcode.escapeplan;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class escape110 {
     boolean isBalanced = true;
     public boolean isBalanced(TreeNode root) {
@@ -18,5 +22,59 @@ public class escape110 {
         TreeNode left;
         TreeNode right;
         TreeNode(int x) { val = x; }
+    }
+}
+
+
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> pool = new HashSet<>(wordList);
+        if (!pool.contains(endWord))
+            return 0;
+
+        Set<String> beginWords = new HashSet<>();
+        Set<String> endWords = new HashSet<>();
+        beginWords.add(beginWord);
+        endWords.add(endWord);
+
+        return bfs(beginWords, endWords, pool);
+    }
+
+    private int bfs(Set<String> beginWords, Set<String> endWords, Set<String> pool) {
+        if (beginWords.size() == 0)
+            return 0;
+
+        pool.removeAll(beginWords);
+        Set<String> temp = new HashSet<>();
+
+        for (String s : beginWords) {
+            char[] chars = s.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                char pre = chars[i];
+                for (char c = 'a'; c <= 'z'; c++) {
+                    if (c == pre)
+                        continue;
+
+                    chars[i] = c;
+                    String temp_str = new String(chars);
+                    if (pool.contains(temp_str)) {
+                        if (endWords.contains(temp_str))
+                            return 2;
+
+                        temp.add(temp_str);
+                    }
+                }
+                chars[i] = pre;
+            }
+        }
+
+        int res;
+        if (temp.size() <= endWords.size()) {
+            res = bfs(temp, endWords, pool);
+        } else {
+            res = bfs(endWords, temp, pool);
+        }
+
+        return res == 0 ? 0 : res + 1;
     }
 }
