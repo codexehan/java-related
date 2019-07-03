6b04494e4aac3987025dff2778cbaee22c96dbf7
Zhenchao Bing <bingzhenchao@mozat.com>
2019-07-03 10:11:00 +0800

    daily escape

---
+++ leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape131.java
@@ -0,0 +1,44 @@
+package codexe.han.leetcode.escapeplan;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 回溯算法
+ * 遍历每个子串，
+ * abcba
+ * a
+ * ab
+ * abc
+ * abcd
+ * abcba
+ */
+public class escape131 {
+    public List<List<String>> partition(String s) {
+        List<List<String>> finalRes = new ArrayList<>();
+        List<String> res = new ArrayList<>();
+        backtracking(s,finalRes, res, 0);
+        return finalRes;
+    }
+
+    public void backtracking(String s, List<List<String>> finalRes, List<String>res, int start){
+        if(start==s.length()) {
+            finalRes.add(new ArrayList<>(res));
+            return;
+        }
+        for(int i=start; i<s.length(); i++){
+            if(isPalindrom(s,start,i+1)){
+                res.add(s.substring(start, i+1));
+                backtracking(s,finalRes,res,i+1);
+                res.remove(res.size()-1);
+            }
+        }
+    }
+
+    public boolean isPalindrom(String s, int start, int end){
+        while(start<end){
+            if(s.charAt(start++)!=s.charAt(end++)) return false;
+        }
+        return true;
+    }
+}
