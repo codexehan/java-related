183429f46d933dd05483c74d818095aa93404a6a
Zhenchao Bing <bingzhenchao@mozat.com>
2019-08-02 22:59:07 +0800

    daily escape

--- elasticsearch/src/main/java/codexe/han/elasticsearch/test/ReadExcel.java
+++ elasticsearch/src/main/java/codexe/han/elasticsearch/test/ReadExcel.java
@@ -7,17 +7,24 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ReadExcel {
     /**
      * 读取xls文件
      * @throws IOException
      */
-    public static void readXls() throws IOException
+    public static void readXls(String path) throws IOException
     {
-        FileInputStream in = new FileInputStream("/Users/zhenchao/Documents/influencer_synonyms_20190524.xls");
+        FileInputStream in = new FileInputStream(path);
         HSSFWorkbook book = new HSSFWorkbook(in);
 
         HSSFSheet sheet = book.getSheetAt(0);
@@ -40,9 +47,9 @@ public class ReadExcel {
      * 读取xlsx文件
      * @throws IOException
      */
-    public static void readXlsx() throws IOException
+    public static void readXlsx(String path) throws IOException
     {
-        XSSFWorkbook xwb = new XSSFWorkbook("/Users/zhenchao/Documents/influencer_synonyms_20190524.xlsx");
+        XSSFWorkbook xwb = new XSSFWorkbook(path);
         XSSFSheet sheet = xwb.getSheetAt(0);
         XSSFRow row;
         String cell;
@@ -83,8 +90,43 @@ public class ReadExcel {
 
     public static void main(String[] args) throws IOException
     {
-        //readXls();
-        readXlsx();
+      //  String path = "/Users/zhenchao/Documents/influencer_synonyms_20190524.xls";
+
+      //  readXlsx(path);
+        String path = "/Users/zhenchao/Downloads/0730/n&adj.csv";
+        readCsv(path);
     }
 
+
+    public static void readCsv(String path){
+        Path pathToFile = Paths.get(path);
+
+        // create an instance of BufferedReader
+        // using try with resource, Java 7 feature to close resources
+        try (BufferedReader br = Files.newBufferedReader(pathToFile,
+                StandardCharsets.US_ASCII)) {
+
+            // read the first line from the text file
+            String line = br.readLine();
+
+            // loop until all lines are read
+            while (line != null) {
+
+                // use string.split to load a string array with the values from
+                // each line of
+                // the file, using a comma as the delimiter
+                line=line.replace("\"","");
+                String[] attributes = line.split(",");
+                System.out.println(Arrays.toString(attributes));
+
+                // read next line before looping
+                // if end of file reached, line would be null
+                line = br.readLine();
+            }
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+    }
 }
