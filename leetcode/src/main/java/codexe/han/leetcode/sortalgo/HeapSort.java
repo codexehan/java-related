87712a4c1b1077fd8b9e5c7c2de3be3aa61ec733
Zhenchao Bing <bingzhenchao@mozat.com>
2019-09-18 21:53:10 +0800

    docker

--- leetcode/src/main/java/codexe/han/leetcode/sortalgo/HeapSort.java
+++ leetcode/src/main/java/codexe/han/leetcode/sortalgo/HeapSort.java
@@ -13,6 +13,12 @@ import java.util.Arrays;
  * 大顶堆数组：arr[i]>=arr[2i+1] && arr[i]>=arr[2i+2]   2i+1 2i+2就是节点的左右孩子节点
  * 小顶堆：每个节点的值都小于或等于其左右孩子节点的值
  * 小顶堆数组：arr[i]<=arr[2i+1] && arr[i]<=arr[2i+2]
+ *
+ * 建堆算法复杂度 O(n)
+ * 建堆算法是从最后一个非叶子结点开始下溯(即 Max-Heapify操作)
+ * 也可以把建堆过程想成先对左子树建堆(T(n/2))，再对右子树建堆(T(n/2))
+ * 最后对根下溯(O(lg n))，所以递推式是T(n) = 2*T(n/2) + O(lg n)它的解是 T(n) = O(n)
+ *
  */
 public class HeapSort {
     public static void main(String[] args) {
@@ -38,12 +44,14 @@ public class HeapSort {
     public static void adjustHeap(int[] arr, int i, int length){
         int tmp = arr[i];
         for(int k=2*i+1;k<length;k=2*k+1){//从i节点的左子节点开始，也就是2i+1处开始
+            //找到左右子节点当中的最大值 k指向该节点
             if(k+1<length && arr[k]<arr[k+1]){//如果左子节点小于右子节点，k指向右子节点
                 k++;
             }
+            //比较确定是否需要交换
             if(arr[k]>tmp){//如果子节点大于父节点，将子节点赋值给父节点（不用进行交换）
                 arr[i] = arr[k];
-                i=k;
+                i=k;//交换以后继续向下交换
             }
             else{
                 break;
