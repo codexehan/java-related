3b4874eb4434c042dbbde2728f7dd3ec50bca860
codexe <zhenchaobing@gmail.com>
2019-09-21 22:19:33 +0800

    daily escape

---
+++ leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape155.java
@@ -0,0 +1,8 @@
+package codexe.han.leetcode.escapeplan;
+
+/**
+ * stack中每个位置都保存数值与当前最小值的差值，小于负数的时候，说明是最小值交界处，需要更换最小值
+ */
+public class escape155 {
+
+}
---
+++ leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape160.java
@@ -0,0 +1,31 @@
+package codexe.han.leetcode.escapeplan;
+
+/**
+ * 如果list1长度是a+c list2长度是b+c
+ * 如果a先遍历list1 a+c 再转到list2 b+c a+c+b+c
+ * 如果b先遍历list2 b+c 再转到list1 a+c b+c+a+c
+ *
+ * a+c+b = b+c+a 所以两个指针肯定会在这个位置遇见，假设有交集的话
+ * 如果c=0 就是没有交集null， 连个指针在结尾处遇见
+ *
+ */
+public class escape160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA==null||headB==null) return null;
+        ListNode a = headA;
+        ListNode b = headB;
+        while(a!=b){
+            a = a==null?headB:a.next;
+            b = b==null?headA:b.next;
+        }
+        return a;
+    }
+    class ListNode {
+          int val;
+         ListNode next;
+         ListNode(int x) {
+             val = x;
+             next = null;
+         }
+    }
+}
