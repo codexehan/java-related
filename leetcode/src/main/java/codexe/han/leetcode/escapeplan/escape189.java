2035dc279eb010e729532b3a76739db86859b84f
codexe <zhenchaobing@gmail.com>
2019-09-23 22:40:07 +0800

    daily escape

--- leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape189.java
+++ leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape189.java
@@ -1,7 +1,24 @@
 package codexe.han.leetcode.escapeplan;
 
+/**
+ * 把数组后k个数，放到数组的前k个位置
+ *
+ * k位置分为两部分
+ * 两边各自做首尾对调
+ * 然后整个做首尾对调
+ */
 public class escape189 {
     public void rotate(int[] nums, int k) {
-
+        k=k%nums.length;//大于length 取模
+        reverse(nums, 0, nums.length - k - 1);
+        reverse(nums, nums.length - k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
+    }
+    public void reverse(int[] nums, int left, int right){
+        while(left<right){
+            int tmp = nums[left];
+            nums[left++]=nums[right];
+            nums[right--]=tmp;
+        }
     }
 }
--- leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape198.java
+++ leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape198.java
@@ -1,7 +1,10 @@
 package codexe.han.leetcode.escapeplan;
 
 /**
- * 不相邻的数相加之和最大
+ * 不相邻的数相加之和最大  但是  中间有可能横跨多个相加数值最大
+ * 比如[2,1,1,2]
+ * 所以要维护该节点之前的所有节点中的最大值
+ *
  * Input: [2,7,9,3,1]
  * Output: 12
  * Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
@@ -11,11 +14,13 @@ public class escape198 {
     public int rob(int[] nums) {
         int[] dp = new int[nums.length+2];
         int max = 0;
+        int subMax = 0;
         dp[0] = 0;
         dp[1] = 0;
         for(int i=0; i<nums.length;i++){
-            dp[i+2] = dp[i] + nums[i+2];
+            dp[i+2] = subMax+nums[i];
             max = Math.max(dp[i+2], max);
+            subMax = Math.max(subMax,dp[i+1]);
         }
         return max;
     }
