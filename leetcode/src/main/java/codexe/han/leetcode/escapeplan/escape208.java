07be904d1f4cc99d049179f4ff3a294f81dae3bd
codexe <zhenchaobing@gmail.com>
2019-09-30 22:39:30 +0800

    daily escape

---
+++ leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape208.java
@@ -0,0 +1,82 @@
+package codexe.han.leetcode.escapeplan;
+
+public class escape208 {
+
+}
+class TrieNode{
+    TrieNode[] nextNodes;
+    boolean isWord;
+}
+class Trie {
+
+    private TrieNode root;
+    /** Initialize your data structure here. */
+    public Trie() {
+        this.root = new TrieNode();
+        this.root.nextNodes = new TrieNode[26];
+        for(int i=0;i<26;i++){
+            this.root.nextNodes[i] = null;
+        }
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode node = root;
+        for(int i=0;i<word.length();i++){
+            int n = word.charAt(i) - 'a';
+            if(node.nextNodes[n]==null){
+                node.nextNodes[n] = new TrieNode();
+                if(!node.nextNodes[n].isWord) node.nextNodes[n].isWord = i==word.length()-1;//这里要注意，一定不要覆盖之前已经插入的isWord
+                node.nextNodes[n].nextNodes = new TrieNode[26];
+                for(int j=0;j<26;j++){
+                    node.nextNodes[n].nextNodes[j] = null;
+                }
+            }
+            /*else{
+                node.nextNodes[n].nextNodes = new TrieNode[26];
+            }*/
+            node = node.nextNodes[n];
+
+        }
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode node = root;
+        for(int i=0;i<word.length();i++){
+            int n = word.charAt(i) - 'a';
+            if(node.nextNodes[n]!=null){
+                if(i==word.length()-1){
+                        return node.nextNodes[n].isWord;
+                }
+                else{
+                    node = node.nextNodes[n];
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        return false;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for(int i=0;i<prefix.length();i++){
+            int n = prefix.charAt(i) - 'a';
+            if(node.nextNodes[n]!=null){
+                if(i==prefix.length()-1){
+                    return true;
+                }
+                else{
+                    node = node.nextNodes[n];
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        return false;
+    }
+}
--- leetcode/src/main/java/codexe/han/leetcode/multithread/proconcache.java
+++ leetcode/src/main/java/codexe/han/leetcode/multithread/proconcache.java
@@ -4,40 +4,79 @@ package codexe.han.leetcode.multithread;
  * Thread safe cache
  */
 public class proconcache {
-    private int[] cache;
-    private int currentSize;
-    public proconcache(int capacity){
-        this.cache = new int[capacity];
-        this.currentSize = 0;
-    }
 
-    /**
-     * 生产
-     * @return
-     */
-    public boolean put(int n) throws InterruptedException {
-        //1.缓存满了的话，需要阻塞
-        if(this.currentSize==this.cache.length){
-            wait();
-        }
-        else{
-            currentSize++;
-            this.cache[currentSize-1] = n;
-            notifyAll();
-            return true;
-        }
-    }
 }
 
 class BaseCache<T>{
     private int head;
+    private int tail;
     private int currentSize;
     private T[] cache;
     public BaseCache(int capacity){
         this.cache = (T[]) new Object[capacity];
-        this.currentSize = 0;
-        this.head = 0;
     }
 
-    public
+    protected synchronized void doPut(T t){
+        this.cache[tail++] = t;
+        if(tail == this.cache.length-1){
+            tail = 0;
+        }
+        currentSize++;
+    }
+
+    protected synchronized T doTake(){
+        T t = this.cache[head];
+        this.cache[head] = null;
+        head++;
+        if(head == this.cache.length-1){
+            head = 0;
+        }
+        currentSize--;
+        return t;
+    }
+
+    protected synchronized boolean isFull(){
+        return currentSize==this.cache.length;
+    }
+
+    protected synchronized boolean isEmpty(){
+        return currentSize == 0;
+    }
+}
+
+class CacheThreadSafe<T> extends BaseCache<T>{
+
+    public CacheThreadSafe(int capacity) {
+        super(capacity);
+    }
+
+    /**
+     * 只有两种情况，会wait，要么是满了，要么是空了
+     * 所以，只有当满->不满，唤醒
+     * 空->不空，唤醒
+     */
+
+    public synchronized void put(T t) throws InterruptedException {
+        //条件为此 是否满
+        while(isFull()){
+            wait();
+        }
+        boolean wasEmpty = isEmpty();
+        doPut(t);
+        if(wasEmpty){//空->非空
+            notifyAll();
+        }
+    }
+
+    public synchronized T take() throws InterruptedException {
+        while(isEmpty()){
+            wait();
+        }
+        boolean wasFull = isFull();
+        T t = doTake();
+        if(wasFull){//满->不满
+            notifyAll();
+        }
+        return t;
+    }
 }
