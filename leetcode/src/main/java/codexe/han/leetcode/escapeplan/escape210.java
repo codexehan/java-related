bf2d2f54fc3273c4e10060f3120316b07f07d5b2
Zhenchao Bing <bingzhenchao@mozat.com>
2019-10-03 09:51:31 +0800

    daily escape

--- leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape210.java
+++ leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape210.java
@@ -1,12 +1,13 @@
 package codexe.han.leetcode.escapeplan;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class escape210 {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         int[] du = new int[numCourses];
-        List[] courses = new ArrayList[numCourses];
+        List<Integer>[] courses = new ArrayList[numCourses];
         for(int i=0;i<numCourses;i++){
             courses[i] = new ArrayList();
         }
@@ -14,7 +15,25 @@ public class escape210 {
             du[prerequisites[i][1]]++;
             courses[prerequisites[i][0]].add(prerequisites[i][1]);
         }
+        LinkedList<Integer> queue = new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(du[i]==0){
+                queue.add(i);
+            }
+        }
 
-
+        int[] res = new int[numCourses];
+        int i=0;
+        while(!queue.isEmpty()){
+            int courseDu0 = queue.pollFirst();
+            res[i++] = courseDu0;
+           //update du of all the followers
+            for(int course : courses[courseDu0]){
+                if(--du[course]==0){
+                    queue.add(course);
+                }
+            }
+        }
+        return res;
     }
 }
