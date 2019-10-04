1f46a4eb5c6c7fae2d3ac6bf1e32c929a7226f8a
codexe <zhenchaobing@gmail.com>
2019-10-04 22:55:36 +0800

    daily escape

--- leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape210.java
+++ leetcode/src/main/java/codexe/han/leetcode/escapeplan/escape210.java
@@ -1,19 +1,24 @@
 package codexe.han.leetcode.escapeplan;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class escape210 {
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
+    public static void main(String[] args) {
+        int[] res = new int[2];
+        System.out.println(res[0]);
+    }
+    public static int[] findOrder(int numCourses, int[][] prerequisites) {
         int[] du = new int[numCourses];
         List<Integer>[] courses = new ArrayList[numCourses];
         for(int i=0;i<numCourses;i++){
             courses[i] = new ArrayList();
         }
         for(int i=0;i<prerequisites.length;i++){
-            du[prerequisites[i][1]]++;
-            courses[prerequisites[i][0]].add(prerequisites[i][1]);
+            du[prerequisites[i][0]]++;
+            courses[prerequisites[i][1]].add(prerequisites[i][0]);
         }
         LinkedList<Integer> queue = new LinkedList<>();
         for(int i=0;i<numCourses;i++){
@@ -22,11 +27,11 @@ public class escape210 {
             }
         }
 
-        int[] res = new int[numCourses];
-        int i=0;
+        //int[] res = new int[numCourses];//不能用数组 会输出[0,0]初始化状态
+        List<Integer> res = new ArrayList<>();
         while(!queue.isEmpty()){
             int courseDu0 = queue.pollFirst();
-            res[i++] = courseDu0;
+            res.add(courseDu0);
            //update du of all the followers
             for(int course : courses[courseDu0]){
                 if(--du[course]==0){
@@ -34,6 +39,8 @@ public class escape210 {
                 }
             }
         }
-        return res;
+        //list to array 的操作会特别耗时，优化方式是使用数组，空new int[0]
+        if(res.size()<numCourses) res.clear();
+        return res.stream().mapToInt(Integer::intValue).toArray();
     }
 }
